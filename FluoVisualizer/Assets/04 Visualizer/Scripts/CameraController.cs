4d5227f1b765fa06eecbc058de80a439bdff5eb5
Keijiro Takahashi <keijiro@gmail.com>
2025-09-22 22:12:12 +0900

    Refactoring to CameraController

--- FluoVisualizer/Assets/04 Visualizer/Scripts/CameraController.cs
+++ FluoVisualizer/Assets/04 Visualizer/Scripts/CameraController.cs
@@ -42,6 +42,7 @@ public sealed class CameraController : MonoBehaviour
     struct AngleSetting
     {
         public float baseAngle;
+        public float baseOffset;
         public Vector3 swingRange;
     }
 
@@ -53,45 +54,44 @@ public sealed class CameraController : MonoBehaviour
 
     #region Parameter application
 
+    (int i0, int i1, float t) GetLerpParams(float parameter, int count)
+    {
+        parameter *= count - 1;
+        var i0 = (int)math.floor(parameter);
+        var i1 = math.min(i0 + 1, count - 1);
+        return (i0, i1, parameter - i0);
+    }
+
     void ApplyDistanceSettings(float parameter)
     {
-        var count = _distanceSettings.Length;
-        parameter *= count - 1;
-        var index0 = (int)math.floor(parameter);
-        var index1 = math.min(index0 + 1, count - 1);
-        ApplyLerpedDistanceSettings(index0, index1, parameter - index0);
+        var p = GetLerpParams(parameter, _distanceSettings.Length);
+
+        ref var s0 = ref _distanceSettings[p.i0];
+        ref var s1 = ref _distanceSettings[p.i1];
+
+        var range = math.lerp(s0.distanceMinMax, s1.distanceMinMax, p.t);
+        var slide = math.lerp(s0.slideRange, s1.slideRange, p.t);
+
+        var  z = (range.x + range.y) * -0.5f;
+        var dz = (range.y - range.x) *  0.5f;
+
+        _targetMotion.positionAmount = math.float3(slide, 0).xzy;
+        _distanceBase.localPosition = new Vector3(0, 0, z);
+        _distanceMotion.positionAmount = new Vector3(0, 0, dz);
     }
 
     void ApplyAngleSettings(float parameter)
     {
-        var count = _angleSettings.Length;
-        parameter *= count - 1;
-        var index0 = (int)math.floor(parameter);
-        var index1 = math.min(index0 + 1, count - 1);
-        ApplyLerpedAngleSettings(index0, index1, parameter - index0);
-    }
+        var p = GetLerpParams(parameter, _angleSettings.Length);
 
-    void ApplyLerpedDistanceSettings(int index0, int index1, float t)
-    {
-        ref var setting0 = ref _distanceSettings[index0];
-        ref var setting1 = ref _distanceSettings[index1];
+        ref var s0 = ref _angleSettings[p.i0];
+        ref var s1 = ref _angleSettings[p.i1];
 
-        var minmax = math.lerp(setting0.distanceMinMax, setting1.distanceMinMax, t);
-        var (z, dz) = ((minmax.x + minmax.y) * -0.5f, (minmax.y - minmax.x) * 0.5f);
+        var baseAngle = math.lerp(s0.baseAngle, s1.baseAngle, p.t);
+        var baseOffset = math.lerp(s0.baseOffset, s1.baseOffset, p.t);
+        var swingRange = math.lerp(s0.swingRange, s1.swingRange, p.t);
 
-        _distanceBase.localPosition = new Vector3(0, 0, z);
-        _distanceMotion.positionAmount = new Vector3(0, 0, dz);
-        _targetMotion.positionAmount = math.float3(math.lerp(setting0.slideRange, setting1.slideRange, t), 0).xzy;
-    }
-
-    void ApplyLerpedAngleSettings(int index0, int index1, float t)
-    {
-        ref var setting0 = ref _angleSettings[index0];
-        ref var setting1 = ref _angleSettings[index1];
-
-        var baseAngle = math.lerp(setting0.baseAngle, setting1.baseAngle, t);
-        var swingRange = math.lerp(setting0.swingRange, setting1.swingRange, t);
-
+        _targetBase.localPosition = new Vector3(0, 0, -baseOffset);
         _rotationBase.localRotation = Quaternion.AngleAxis(90 - baseAngle, Vector3.right);
         _rotationMotion.rotationAmount = swingRange;
     }
@@ -99,9 +99,6 @@ public sealed class CameraController : MonoBehaviour
     #endregion
 
     #region MonoBehaviour implementation
-
-    void Start()
-      => _targetBase.position = Vector3.zero; // FIXME: Just for suppressing the warning.
 
     void Update()
     {
