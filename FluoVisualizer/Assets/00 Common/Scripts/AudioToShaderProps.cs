1b9d6eed25838d94b416b1fca5dfafbeeb319493
Keijiro Takahashi <keijiro@gmail.com>
2025-10-08 22:11:49 +0900

    Audio In controls

--- FluoVisualizer/Assets/00 Common/Scripts/AudioToShaderProps.cs
+++ FluoVisualizer/Assets/00 Common/Scripts/AudioToShaderProps.cs
@@ -5,6 +5,9 @@ namespace Fluo {
 
 public sealed class AudioToShaderProps : MonoBehaviour
 {
+    [field:SerializeField, Range(0, 1)]
+    public float Amplitude { get; set; } = 1;
+
     [SerializeField] AudioLevelTracker _trackerX = null;
     [SerializeField] AudioLevelTracker _trackerY = null;
     [SerializeField] AudioLevelTracker _trackerZ = null;
@@ -16,7 +19,7 @@ public sealed class AudioToShaderProps : MonoBehaviour
         var y = _trackerY.normalizedLevel;
         var z = _trackerZ.normalizedLevel;
         var w = _trackerW.normalizedLevel;
-        Shader.SetGlobalVector(ShaderID.FluoAudioLevel, new Vector4(x, y, z, w));
+        Shader.SetGlobalVector(ShaderID.FluoAudioLevel, new Vector4(x, y, z, w) * Amplitude);
     }
 }
 
