21d56cfdb52da31286da3068b4eb01623e85fabb
Keijiro Takahashi <keijiro@gmail.com>
2025-10-10 15:45:02 +0900

    Duotone controllers

--- FluoVisualizer/Assets/02 Brush/Scripts/BrushController.cs
+++ FluoVisualizer/Assets/02 Brush/Scripts/BrushController.cs
@@ -1,9 +1,5 @@
 using UnityEngine;
 using UnityEngine.VFX;
-using Unity.Mathematics;
-using Random = UnityEngine.Random;
-
-using System;
 
 namespace Fluo {
 
@@ -18,13 +14,11 @@ public sealed class BrushController : MonoBehaviour
         {
             var t = (i + 1.0f) / _colorKeys.Length;
 
-            var h = Random.value * math.PI * 2;;
+            var h = Random.value * Mathf.PI * 2;
             var s = 100.0f;
             var v = t * t * t * 59 + 1;
 
-            HsluvHelper.HsluvToRgb(math.float3(h, s, v), out float3 rgb);
-            var c = new Color(rgb.x, rgb.y, rgb.z);
-
+            var c = Pugrad.Hsluv.ToRgb(h, s, v);
             _colorKeys[i] = new GradientColorKey(c, t);
         }
 
